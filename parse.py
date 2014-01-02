4ad2f66ed2c25edee369c23bd274833e952f4bb9
BlackMoses <BlackMosesOrg@gmail.com>
2014-01-03 00:53:13 +0100

    very simple code execution from file

--- parse.py
+++ parse.py
@@ -6,18 +6,41 @@ Written entirely by BlackMoses; blackmoses.org
 
 Very hacky :)
 Simplicity over complexity.
+
+
+For now using Python's lists as native type. Maybe I'll migrate to already
+implemented linkedlists.
 '''
 
+ATOMS = (float, int, str, unicode)
 
-class InterpreterError(Exception):
+# Built-in functions:
+def a():
+    print 'I am a and have just been called'
+    return 'a'
+
+def b(*args):
+    print 'I am b and have just been called with args: %s' % repr(args)
+
+BUILT_INS = {
+    'a': a,
+    'b': b
+}
+
+
+class LanguageError(Exception):
     pass
 
 
-class TokenizerError(InterpreterError):
+class TokenizerError(LanguageError):
     pass
 
 
-class ParserError(InterpreterError):
+class ParserError(LanguageError):
+    pass
+
+
+class InterpreterError(LanguageError):
     pass
 
 
@@ -42,7 +65,7 @@ def tokenize(code):
             # Valid types are Python's float, int, str, unicode. Those types
             # are managed by Python's runtime.
             atom = eval(repr(token), {})
-            if not isinstance(atom, (float, int, str, unicode)):
+            if not isinstance(atom, ATOMS):
                 raise TokenizerError(
                     'Token %s is %s. Must be float, int, str or unicode.' %
                     (token, type(token)))
@@ -84,8 +107,62 @@ def parse(tokens):
             else:
                 output.append(tokens[index])
             index += 1
-        raise RuntimeError('S-expression is not closed.')
+        raise ParserError('S-expression is not closed.')
 
     if tokens[0] != '(':
-        raise RuntimeError('S-expression is not opened.')
+        raise ParserError('S-expression is not opened.')
     return do_parse(tokens[1:])[0]
+
+
+def interpret(i_list):
+    ''' Recursively evaluate lists. '''
+    # First element is the function name.
+    # The rest are args.
+    i_globals = BUILT_INS  # Currently code supports only globals.
+    # Turing-completeness shall be guaranteed by built-in functions.
+
+    try:
+        fn_name = i_list[0]
+        fn = i_globals[fn_name]
+    except IndexError:
+        raise InterpreterError('Empty list call')
+    except KeyError:
+        raise InterpreterError('Attempted to call unexisting function.')
+
+    args = []
+    for el in i_list[1:]:
+        if isinstance(el, ATOMS):
+            args.append(el)
+        elif isinstance(el, list):
+            args.append(interpret(el))
+        else:
+            raise RuntimeError('Incompatible type in interpret: %s. '
+                        'Should never happen.' % type(el))
+
+    return fn(*args)
+
+def run(code):
+    try:
+        tokens = tokenize(code)
+        parsed = parse(tokens)
+        print interpret(parsed)
+        return 0
+    except LanguageError as err:
+        print repr(err)
+        return -1
+
+
+def main():
+    import io, sys
+    try:
+        code = io.open(sys.argv[1]).read()
+    except IndexError:
+        print 'File name required.'
+        return -2
+    except IOError:
+        print 'Cannot open file: \'%s\'.' % sys.argv[1]
+    return run(code)
+
+
+if __name__ == '__main__':
+    main()
